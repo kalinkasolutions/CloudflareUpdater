987ba093bb05de5fcd1cd378bf04237b846b4b1e
kalinka <kalinka@kalinka.ch>
2026-02-05 10:50:17 +0100

    fix: don't update previous ip on error

--- update_cloudflare.py
+++ update_cloudflare.py
@@ -4,39 +4,86 @@ import json
 import logging
 import sys
 
+
 def get_current_ip():
     try:
-        ip_request = requests.get(config_data["ip_address_provider_url"])
+        ip_request = requests.get(config_data["IP_ADDRESS_PROVIDER_ENDPOINT"])
         ip_request.raise_for_status()
         return ip_request.text
     except requests.exceptions.RequestException as e:
-        logging.error(f"Failed to get current IP: {e}")
+        logging.error(
+            f"Failed to get current IP from provider {config_data["IP_ADDRESS_PROVIDER_ENDPOINT"]}: {e}"
+        )
         sys.exit(1)
+
 
 def should_update_dns_record(dns_record, current_ip):
     return (
-        dns_record["name"] not in excluded_dns_records and
-        dns_record["content"] != current_ip and
-        not ipaddress.ip_address(dns_record["content"]).is_private
+        dns_record["name"] not in excluded_dns_records
+        and dns_record["content"] != current_ip
+        and not ipaddress.ip_address(dns_record["content"]).is_private
     )
 
-def update_dns_records_for_zone(zone, authorization_headers, current_ip):
+
+def try_get_dns_records(zone, authorization_headers):
     try:
-        dns_records_json = requests.get(
-            f"https://api.cloudflare.com/client/v4/zones/{zone['zone_id']}/dns_records?type=A", headers=authorization_headers).text
-        dns_records = json.loads(dns_records_json)
-        for dns_record in dns_records["result"]:
-            if should_update_dns_record(dns_record, current_ip):
-                logging.info(f"updating record: {dns_record['name']} with ip: {current_ip}")
-                requests.patch(f"https://api.cloudflare.com/client/v4/zones/{zone['zone_id']}/dns_records/{dns_record['id']}", headers=authorization_headers, json={
-                    "type": "A", "name": dns_record["name"], "content": current_ip, "ttl": 120})
+        response = requests.get(
+            f"https://api.cloudflare.com/client/v4/zones/{zone['zone_id']}/dns_records?type=A",
+            headers=authorization_headers,
+        )
+        response.raise_for_status()
+        return True, json.loads(response.text)
     except Exception as e:
-        logging.error(f"Failed to update DNS records for zone {zone['name']}: {e}")
+        logging.error(
+            f"Failed to fetch dns records for zone id: {zone['zone_id']}: {e}"
+        )
+        return False, None
 
 
-logging.basicConfig(filename='log.log', level=logging.INFO)
+def update_dns_records_for_zone(zone, authorization_headers, current_ip):
+    update_results = []
 
-with open('config.json') as config_file:
+    success, dns_records = try_get_dns_records(zone, authorization_headers)
+    if not success:
+        return False
+
+    for dns_record in dns_records["result"]:
+        if should_update_dns_record(dns_record, current_ip):
+            logging.info(
+                f"Trying to update record: {dns_record['name']} with IP: {current_ip}"
+            )
+            try:
+                requests.patch(
+                    f"https://api.cloudflare.com/client/v4/zones/{zone['zone_id']}/dns_records/{dns_record['id']}",
+                    headers=authorization_headers,
+                    json={
+                        "type": "A",
+                        "name": dns_record["name"],
+                        "content": current_ip,
+                        "ttl": 120,
+                    },
+                )
+                logging.info(
+                    f"Updated record: {dns_record['name']} with IP: {current_ip}"
+                )
+                update_results.append(True)
+            except Exception as e:
+                logging.error(
+                    f"Failed to update DNS records {dns_record['name']} with IP: {current_ip}: {e}"
+                )
+                update_results.append(False)
+
+    return all(update_results)
+
+
+logging.basicConfig(
+    filename="log.log",
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+with open("config.json") as config_file:
     config_data = json.load(config_file)
 
 ip_file_path = "./previous_ip"
@@ -52,14 +99,18 @@ current_ip = get_current_ip()
 if current_ip == previous_ip:
     sys.exit(0)
 
-for config in config_data["cloudflare_configs"]:
-    zones = config["zones"]
-    cloudflare_api_token = config["cloudflare_api_token"]
-    authorization_headers = {'Authorization': f'Bearer {cloudflare_api_token}'}
-    excluded_dns_records = set(config["excluded_dns_records"])
+update_success = []
+for config in config_data["CLOUDFLARE_CONFIGS"]:
+    zones = config["ZONES"]
+    cloudflare_api_token = config["CLOUDFLARE_DNS_API_TOKEN"]
+    authorization_headers = {"Authorization": f"Bearer {cloudflare_api_token}"}
+    excluded_dns_records = set(config["EXCLUDED_DNS_RECORD_NAMES"])
 
     for zone in zones:
-        update_dns_records_for_zone(zone, authorization_headers, current_ip)
+        update_success.append(
+            update_dns_records_for_zone(zone, authorization_headers, current_ip)
+        )
 
-with open(ip_file_path, "w") as f:
-        f.write(current_ip)
+    if all(update_success):
+        with open(ip_file_path, "w") as f:
+            f.write(current_ip)
